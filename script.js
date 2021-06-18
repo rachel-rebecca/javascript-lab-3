2038a65e4f4d61c2e9ecba7228e6c552bbb2c0fe
rachel-rebecca <Rarebecc@umich.edu>
2021-06-18 19:57:48 -0400

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 
 let submissions = [ 
@@ -50,9 +50,9 @@ addSubmission(submissions, "Jay", 85, "2021-03-23");
 
 // Delete by index function
 function deleteSubmissionByIndex(array, index) {
-    submissions.splice(index, 1);
+    array.splice(index, 1);
     console.log("Submission has been deleted by index. " + JSON.stringify(submissions));
-}
+};
 // call delete function
 deleteSubmissionByIndex(submissions, 4);
 
@@ -60,7 +60,7 @@ deleteSubmissionByIndex(submissions, 4);
 // Delete by name function
 const deleteSubmissionByName = (array, name) => {
     const index = array.findIndex(element => element.name === name) // Thank you, Doug!!!
-    submissions.splice(index, 1);
+    array.splice(index, 1);
     console.log("Submission has been deleted by name. " + JSON.stringify(submissions));
 }
 // call delete by name
@@ -69,8 +69,8 @@ deleteSubmissionByName(submissions, "Jill");
 
 // Edit submission function
 function editSubmission(array, index, score) {
-    submissions[index].score = score;
-    submissions[index].passed = score >= 60;
+    array[index].score = score;
+    array[index].passed = score >= 60;
     console.log("Submission has been edited. " + JSON.stringify(submissions)); 
 }
 // call edit 
@@ -90,17 +90,32 @@ findSubmissionByName(submissions, "Jane");
 // }
 
 
-// Find lowest score function
+// What I originally wrote for find lowest score function
+// function findLowestScore (array) {
+//     let scoreArray = array.map(a => a.score) //creating array of scores
+//     scoreArray.forEach(function(score){
+//          score = Math.min(...scoreArray)
+//          const findLow = array.find(element => element.score === score);
+//         console.log(`The lowest score is: ${JSON.stringify(findLow)}`);
+//         if (scoreArray.length >= 1) {
+//             scoreArray.length = 0;
+//         }
+//     })
+// }
+
+//Find lowest score function
 function findLowestScore (array) {
-    let scoreArray = array.map(a => a.score) //creating array of scores
+    let scoreArray = array.map(a => a.score);
+    let lowestScore = scoreArray[0];
     scoreArray.forEach(function(score){
-        score = Math.min(...scoreArray)
-        const findLow = array.find(element => element.score === score);
-        console.log(`The lowest score is: ${JSON.stringify(findLow)}`);
-        if (scoreArray.length >= 1) {
-            scoreArray.length = 0;
+        if (score < lowestScore) {
+            lowestScore = score;
+        } else {
+
         }
     })
+    let lowestScoreObj = array.find(element => element.score === lowestScore);
+    console.log("The lowest score is: " + JSON.stringify(lowestScoreObj));
 }
 // call lowest score 
 findLowestScore(submissions);
